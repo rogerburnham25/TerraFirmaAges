e017123dadf4bb425072c5531eefc8dbccd9b54c
HailCider <jmtritonejr@gmail.com>
2026-08-16 22:23:33 -0400

    Add files via upload

---
+++ TerraFirmaAgesCoreClient.java
@@ -0,0 +1,26 @@
+package com.terrafirmaagescore;
+
+import com.terrafirmaagescore.client.screen.TownNameScreen;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.common.NeoForge;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = TerraFirmaAgesCore.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+public class TerraFirmaAgesCoreClient {
+    public TerraFirmaAgesCoreClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        NeoForge.EVENT_BUS.register(TownNameScreen.class);
+    }
+}
